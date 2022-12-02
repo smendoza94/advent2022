d83cfe9b9ecd0fd098f32de3b9f9885fa3361719
StevenM <stevenbmendoza@gmail.com>
2022-12-02 00:07:59 -0800

    working on splitting list to sums

--- 1-dec/index.py
+++ 1-dec/index.py
@@ -1,8 +1,19 @@
-# open file in read and read data
-caloriesDataFile = open("caloriesData.txt","r")
+# open file in read mode 
+calories_file = open("caloriesData.txt","r")
 
-caloriesData = caloriesDataFile.read()
+# read file data into a variable
+calories_data = calories_file.read()
 
-# print data
-print(caloriesData)
-caloriesDataFile.close()
+# convert string to list through splitting
+calories_list = calories_data.replace('\n', ' ').split(" ")
+
+# 
+lists =[[]]
+for i in calories_list:
+  if i == ' ':
+    lists.append([])
+  else:
+    lists[-1].append(i)
+
+# close file
+calories_file.close()
