fb1cfaa228686e7230e0ef5f242515fffe3755ce
StevenM <stevenbmendoza@gmail.com>
2022-12-02 18:53:47 -0800

    convert all calories from strings to integers, and seperate each elfs lists into their own array

--- 1-dec/index.py
+++ 1-dec/index.py
@@ -4,16 +4,23 @@ calories_file = open("caloriesData.txt","r")
 # read file data into a variable
 calories_data = calories_file.read()
 
-# convert string to list through splitting
+# replace all the /n base text to a ' ' space
+# then convert string to list through splitting 
+# to be able to manipulate the array  
 calories_list = calories_data.replace('\n', ' ').split(" ")
 
-# 
-lists =[[]]
+# convert the strings in the arry into 
+
+# seperate each elf's listed calories into their own array by using 
+# the empty '' so that we can sum up each total
+elf_calorie_lists =[[]]
 for i in calories_list:
-  if i == ' ':
-    lists.append([])
+  if i == '':
+    elf_calorie_lists.append([])
   else:
-    lists[-1].append(i)
+    i = int(i)
+    elf_calorie_lists[-1].append(i)
+print(elf_calorie_lists)
 
 # close file
 calories_file.close()
