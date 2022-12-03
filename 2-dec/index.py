5a358c070ec7b30ea8a975843b30c4d6775dcd9d
StevenM <stevenbmendoza@gmail.com>
2022-12-02 22:00:49 -0800

    score increase based on hand played

--- 2-dec/index.py
+++ 2-dec/index.py
@@ -8,7 +8,16 @@ strat_data = strat_file.read()
 # then convert string to list through splitting 
 # to be able to manipulate the array  
 strat_list = strat_data.replace('\n', ' ').split(" ")
-print(strat_list)
 
+my_score = 0
+for i in strat_list:
+  if i == 'X':
+    my_score += 1
+  if i == 'Y':
+    my_score += 2
+  if i == 'Z':
+    my_score += 3
+
+print(my_score)
 # close file
 strat_file.close()
