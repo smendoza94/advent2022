327b4ec57f283b7c3259c60be034fd8342ae0639
StevenM <stevenbmendoza@gmail.com>
2022-12-03 17:55:57 -0800

    add hand point logic, solve and submit

--- 2-dec/README.md
+++ 2-dec/README.md
@@ -44,4 +44,4 @@ Now that you're correctly decrypting the ultra top secret strategy guide, you wo
 
 Following the Elf's instructions for the second column, **what would your total score be if everything goes exactly according to your strategy guide?**
 
-Answer:
+Answer: _13889_
--- 2-dec/index.py
+++ 2-dec/index.py
@@ -42,22 +42,53 @@ for i in matches:
 
 # now sum the total of points from hand and match as my_score and print
 my_score = hand_points + match_points
-print('my total score: ' + str(my_score)) # prints 14827
+print('my intial score: ' + str(my_score)) # prints 14827
 
 #PART 2
 # new conditions were given where X means lose, Y draw, and Z win
+# lose -> [A = Z +3], [B = X +1], [C = Y +2]
+# draw -> [A = X +1], [B = Y +2], [C = Z +3]
+# win -> [A = Y +2], [B = Z +3], [C = X +1]
 new_score = 0
 for i in matches:
   # X means lose +0
   if i[1] == 'X':
     new_score += 0
+    # lose to rock play scissors +3
+    if i[0] == 'A':
+      new_score += 3
+    # lose to paper play rock +1
+    elif i[0] == 'B':
+      new_score += 1
+    # lose to scissors play paper +2
+    elif i[0] == 'C':
+      new_score += 2
   # Y means draw +3
   elif i[1] == 'Y':
     new_score += 3
+    # draw to rock +1
+    if i[0] == 'A':
+      new_score += 1
+    # draw to paper +2
+    elif i[0] == 'B':
+      new_score += 2
+    # draw to scissors +3
+    elif i[0] == 'C':
+      new_score += 3
   # Z means win +6 
   elif i[1] == 'Z':
     new_score += 6
-print('my new score is: ' + str(new_score))
+    # win to rock play paper +2 
+    if i[0] == 'A':
+      new_score += 2
+    # win to paper play scissors +3
+    elif i[0] == 'B':
+      new_score += 3
+    # win to scissors play rock +1
+    elif i[0] == 'C':
+      new_score += 1
+
+print('my new score is: ' + str(new_score)) # points 13889
 
 # close file
 file.close()
