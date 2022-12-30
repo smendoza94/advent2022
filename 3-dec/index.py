088a9f072302a8d49fa0866d398753a2ecb64467
StevenM <stevenbmendoza@gmail.com>
2022-12-29 18:18:46 -0800

    chatGPT: split data in groups of three using zip_longest, and find common item by convert to set and using intersection()

--- 3-dec/index.py
+++ 3-dec/index.py
@@ -1,4 +1,5 @@
 import string
+from itertools import zip_longest
 
 # open file in read mode 
 file = open("contentsData.txt","r")
@@ -36,11 +37,33 @@ print(total_points) # prints 8139
 
 # PART 2
 # divide each groups of 3 rutsucks into their own arrays
-group_rutsacks = []
-temp_group = []
-for i in data:
-  if (data.index(i)+1)%3 == 0:
-    group_rutsacks.append(temp_group)
-    temp_group = []
-  temp_group.append(i)
+# group_rutsacks = []
+# for i in data:
+#   if (data.index(i))%3 == 0:
+#     group_rutsacks.append(temp_group)
+#     temp_group = []
+#   else:
+#     temp_group.append(i)
 
+# this solution was recommended by chatGPT AI and modified by developer
+def group_by_three(array):
+  return zip_longest(*[iter(array)]*3)
+group_rutsacks = [group for group in group_by_three(data) if None not in group]
+
+# compare each groups three rutsacks and find 
+# the common item "letter" between their rutsacks
+# group_common_items = []
+# for i in group_rutsacks:
+#   print(i)
+#   for j in i[0]:
+#     if j in i[1]:
+#       if j in i[2]:        
+#         group_common_items.append([j])
+#         break
+
+# ChatGPT: This code converts each string into a set, 
+# and then uses the intersection method to find the common elements.
+group_common_items = []
+for i in group_rutsacks:
+  group_common_items.append(set(i[0]).intersection(set(i[1])).intersection(set(i[2])))
+print(group_common_items)
